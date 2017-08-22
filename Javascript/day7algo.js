7b15ae5fd662fa307f6537676d087fd72b907f09
Natalia <nataliae.dev@gmail.com>
2017-08-22 16:31:57 -0700

    Edited day7 algorithms for indentation. Created pokemon html file, which pulls from pokeapi.co

--- Javascript/day7algo.js
+++ Javascript/day7algo.js
@@ -8,35 +8,34 @@ function invertHash(object){
         val_list.push(object[key]);
     }
     object= {};//zeros out object
-  for(var i=0; i<key_list.length,i++){
-    object[val_list[i]]=key+list[1];
-}
-return object;
+    for(var i=0; i<key_list.length,i++){
+      object[val_list[i]]=key+list[1];
+    }
+    return object;
 }
 
 //also take into consideration if there are values that are equal and make the new value of their key into an array
 
 function invertHash(object){
-    var key_list=[];
-    var val_list=[];
-    for(var key in object){
-        key_list.push(key);
-        val_list.push(object[key]);
-    }
-    object= {};//zeros out object
-for(i=0;i<key_list.length;i++){
-  if(object[val_list[i]]){
-      if(typeof object[val_list[i]] === "object"){
-          object[val_list[i]].push(key_list[i]);
-      } else {
-          object[val_list[i]] = [object[val_list[i]], key_list[i]]
-      }
+  var key_list=[];
+  var val_list=[];
+  for(var key in object){
+    key_list.push(key);
+    val_list.push(object[key]);
   }
-  else{
-    object[val_list[i]]=key_list[i];
-}
-}
-return object;
+  object= {};//zeros out object
+  for(i=0;i<key_list.length;i++){
+    if(object[val_list[i]]){
+      if(typeof object[val_list[i]] === "object"){
+        object[val_list[i]].push(key_list[i]);
+      } else {
+        object[val_list[i]] = [object[val_list[i]], key_list[i]]
+      }
+    } else{
+      object[val_list[i]]=key_list[i];
+    }
+  }
+  return object;
 }
 
 console.log(invertHash({1:"a", 2:"b", 3:"c", 4:"a"}))
